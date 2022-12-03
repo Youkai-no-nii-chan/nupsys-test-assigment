bada5b616596cfdb2164a3cf56c1a98109a38c4d
Nikolay Andreychuk <youkainoniichan@gmail.com>
2022-12-03 22:26:55 +0000

    gitignore

---
+++ PopulateDatabase/Program.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+
+var cancellationTokenSource = new CancellationTokenSource();
+
+var populateDatabaseTask = PopulateDatabase(cancellationTokenSource.Token);
+
+Console.WriteLine("Populating database with random values...");
+Console.WriteLine("Press any key to exit");
+Console.Read();
+cancellationTokenSource.Cancel();
+Console.WriteLine("Stopping...");
+populateDatabaseTask.Wait();
+Console.WriteLine("Stopped");
+
+async Task PopulateDatabase(CancellationToken cancellationToken)
+{
+    var random = new Random();
+    var dataRepository = new DataRepository();
+
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        var iterationStartDateTimeUtc = DateTime.UtcNow;
+        
+        await dataRepository.InsertAsync(new Data
+        {
+            Timestamp = DateTime.UtcNow,
+            Value = random.NextDouble()
+        });
+
+        await Task.Delay(iterationStartDateTimeUtc.AddSeconds(10) - DateTime.UtcNow);
+    }
+}
---
+++ QueryDatabase/Program.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+using QueryDatabase;
+
+var command = Parser.Default.ParseArguments<
+    FlatListCommand,
+    ConsolidateHoursCommand,
+    ConsolidateMinutesCommand,
+    PeaksPerHourCommand>(args);
+
+if (!command.Errors.Any())
+{
+    Console.WriteLine("Starting export...");
+    await ((ExportCommand) command.Value).Run();
+    Console.WriteLine("Export finished");
+}
